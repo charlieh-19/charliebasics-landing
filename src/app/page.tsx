33d36efdefecf81422f009baf0ad5119676ac1f8
unknown <charliebasicsh@gmail.com>
2024-11-05 14:59:23 -0400

    History

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image"
 
 export default function Home() {
   return (
+  <div>  
     <div className="flex bg-[#0f0f0f] items-center">  
     
         <div className="p-5 text-white items-baseline w-3/6">
@@ -23,7 +24,30 @@ export default function Home() {
           height={200}
         />
         </div>
-    
     </div>
+
+    
+    <div className="flex bg-[#0f0f0f] items-center">  
+      <div className="items-end w-3/6">
+        <Image
+           src="/modelo1.jpg"
+           alt="history-img"
+           className="w-full"
+           width={200}
+           height={200}
+        />
+      </div>
+      
+      <div className="p-5 text-white w-3/6 items-baseline">
+        <h1 className="font-bold text-3xl"
+        >History</h1>
+
+        <p className="pt-2">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lacinia ex suscipit, sodales eros ut, suscipit massa. Nam dictum lacus sit amet ante rhoncus aliquet. Suspendisse nec nunc nec eros iaculis porttitor. Suspendisse condimentum rhoncus ipsum.
+        </p>
+      </div>
+    </div>
+
+  </div>
   );
 }
